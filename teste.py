c176bb2441850a75ddf86aa09469a38ce5920a37
luiseduardomendes <luis.pmendes13@gmail.com>
2023-07-04 17:15:34 -0300

    update

--- teste.py
+++ teste.py
@@ -1,8 +1,18 @@
 import vvcpy as vp 
+import yaml
+from yaml.loader import SafeLoader
 
 cfg = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/VVC_research/database/input_analyser/cfg-files/'
 vtm = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/VVCSoftware_VTM/'
 out = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/vvc_performance/test_output/'
+
+file = 'setup.yaml'
+with open(file) as f:
+    data = yaml.load(f, Loader=SafeLoader)
+
+cfg = data['cfg']
+vtm = data['vtm']
+out = data['out']
 
 sim = vp.sim.Simulation(
     n_frames=32, 
