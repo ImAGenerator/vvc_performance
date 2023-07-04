5fdc06ec3af1007ed5091ac554f52aaca7d0df0d
luiseduardomendes <luismendes@DESKTOP-9H5Q41N.localdomain>
2023-07-04 20:15:37 -0300

    update

--- teste.py
+++ teste.py
@@ -1,18 +1,4 @@
 import vvcpy as vp 
-import yaml
-from yaml.loader import SafeLoader
-
-cfg = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/VVC_research/database/input_analyser/cfg-files/'
-vtm = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/VVCSoftware_VTM/'
-out = '/mnt/c/Users/dudup/OneDrive/Documentos/GitHub/vvc_performance/test_output/'
-
-file = 'setup.yaml'
-with open(file) as f:
-    data = yaml.load(f, Loader=SafeLoader)
-
-cfg = data['cfg']
-vtm = data['vtm']
-out = data['out']
 
 sim = vp.sim.Simulation(
     n_frames=32, 
@@ -21,11 +7,7 @@ sim = vp.sim.Simulation(
     bg_exec=True
 )
 
-sim.set_paths(out, vtm, cfg)
-
-for i in range(8):
-    sim.remove_video_from_buffer(0)
+yaml_file = 'setuphome.yaml'
+sim.read_yaml(yaml_file)
 
 sim.get_exec_info()
-sim.run_exec()
-
