7ab083de43ce5e9bec12c03dd0216d5e8d0223f5
Eduardo <dudabosel@gmail.com>
2023-07-07 10:52:25 -0300

    8x8-1

--- coleta.py
+++ coleta.py
@@ -1,13 +1,18 @@
 import vvcpy as vp 
 
-vtm_dir = '/home/dudabosel/TCC/TCC/'
-cfg_dir = '/home/dudabosel/TCC/configs/'
-out_dir = '/home/dudabosel/TCC/output'
+vtm_dir = '/home/ebbosel/TCC/TCC/' #'/home/dudabosel/TCC/TCC/'
+cfg_dir = '/home/ebbosel/TCC/configs/' #'/home/dudabosel/TCC/configs/'
+out_dir = '/home/ebbosel/TCC/output' #'/home/dudabosel/TCC/output'
 
 encoder = ['AI']
-qps = [22,27,32,37]
+qps = [32]
 
-sim = vp.sim.Simulation(n_frames=60, qps=qps, encoder=encoder, bg_exec=False)
+sim = vp.sim.Simulation(n_frames=60, qps=qps, encoder=encoder, bg_exec=True)
 sim.set_paths(out_dir, vtm_dir, cfg_dir)
+
+new_file = '/home/ebbosel/TCC/aproximacoes/RdCost(8x8)-1.cpp'
+old_file = '/home/ebbosel/TCC/TCC/source/Lib/CommonLib/RdCost.cpp'
+
+sim.change_version("RdCost(8x8)-1", old_file, new_file)
 
 sim.run_exec()
